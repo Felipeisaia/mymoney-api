264cdee83762b62c4c150e56a5e6e29754267547
Felipeisaia <felipeisaia6@gmail.com>
2026-02-14 15:32:04 -0300

    arrumando imports pt2

--- web/src/components/NewTransactionModal.tsx
+++ web/src/components/NewTransactionModal.tsx
@@ -2,6 +2,7 @@ import type { FormEvent } from 'react'
 import Modal from 'react-modal';
 import { useTransactions } from '../hooks/TransactionsContext';
 import { toast } from 'react-toastify';
+import { useState } from 'react';
 
 interface Transaction {
   id: string;
